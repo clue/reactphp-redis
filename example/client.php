926317b4aa898459773ed50efa283edb75e62c12
Christian Lück <christian@lueck.tv>
2013-07-05 19:23:53 +0200

    Add simple client example

---
+++ example/client.php
@@ -0,0 +1,25 @@
+<?php
+
+use Clue\Redis\React\Client;
+
+use Clue\Redis\React\Factory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$loop = React\EventLoop\Factory::create();
+$factory = new React\Dns\Resolver\Factory();
+$resolver = $factory->create('6.6.6.6', $loop);
+$connector = new React\SocketClient\Connector($loop, $resolver);
+$factory = new Factory($loop, $connector);
+
+$factory->createClient()->then(function (Client $client) {
+    $client->incr('test');
+
+    $client->get('test')->then(function ($result) {
+        var_dump($result);
+    });
+
+    $client->end();
+});
+
+$loop->run();
