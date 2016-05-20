ae3c54082b9f440557a93cf71af0cc4db8cca934
Christian Lück <christian@lueck.tv>
2016-05-20 18:06:26 +0200

    Add PubSub examples

---
+++ examples/publish.php
@@ -0,0 +1,23 @@
+<?php
+
+use Clue\React\Redis\Client;
+use Clue\React\Redis\Factory;
+use Clue\Redis\Protocol\Model\StatusReply;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$loop = React\EventLoop\Factory::create();
+$factory = new Factory($loop);
+
+$channel = isset($argv[1]) ? $argv[1] : 'channel';
+$message = isset($argv[2]) ? $argv[2] : 'message';
+
+$factory->createClient()->then(function (Client $client) use ($channel, $message) {
+    $client->publish($channel, $message)->then(function ($received) {
+        echo 'successfully published. Received by ' . $received . PHP_EOL;
+    });
+
+    $client->end();
+});
+
+$loop->run();
---
+++ examples/subscribe.php
@@ -0,0 +1,24 @@
+<?php
+
+use Clue\React\Redis\Client;
+use Clue\React\Redis\Factory;
+use Clue\Redis\Protocol\Model\StatusReply;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$loop = React\EventLoop\Factory::create();
+$factory = new Factory($loop);
+
+$channel = isset($argv[1]) ? $argv[1] : 'channel';
+
+$factory->createClient()->then(function (Client $client) use ($channel) {
+    $client->subscribe($channel)->then(function () {
+        echo 'Now subscribed to channel ' . PHP_EOL;
+    });
+
+    $client->on('message', function ($channel, $message) {
+        echo 'Message on ' . $channel . ': ' . $message . PHP_EOL;
+    });
+});
+
+$loop->run();
